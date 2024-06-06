b925fd5ade942d3db4afc372c3ee53e423a2cd27
arthur andrade <arthurandrad.23@gmail.com>
2024-06-06 19:07:37 -0300

    Style: Styled components in chats page

--- src/Pages/ChatsPage/ChatsPage.tsx
+++ src/Pages/ChatsPage/ChatsPage.tsx
@@ -15,41 +15,48 @@ function ChatsPage({ messages, currentUserId }: MessageProps) {
   const [showAboutContainer, setShowAboutContainer] = useState(false);
   
   useEffect(() => {
-    setShowAboutContainer(true);
+    setShowAboutContainer(false);
   }, []);
 
   return (
     <div className="sections-container">
       <section className='contacts-container'>
-        <header>
-          <div>Chats</div>
-          <div>🔍</div>
+        <header className='d-flex search-container'>
+          <div className='search-about'>Chats</div>
+          <div className='search-button'>🔍</div>
         </header>
-        {/* Example chat */}
-        <div>
-          <img className='chat-picture' src='https://static1.srcdn.com/wordpress/wp-content/uploads/2023/02/nelson-franklin-pointing-his-finger-on-the-office.jpg' alt='nick from the office' />
+        {/* Example contact component */}
+        <div className='contact-container d-flex'>
+          <img className='chat-picture' src='https://hips.hearstapps.com/hmg-prod/images/frenchie-the-boys-season-3-1655812929.jpg?crop=0.253xw:0.380xh;0.580xw,0.0484xh&resize=980:*' alt='nick from the office' />
           <div className='chat-info'>
-            <div className='chat-name'>Indian</div>
-            <div className='chat-message'>Let me fix your computer...</div>
+            <div className='chat-name'>Contact name</div>
+            <div className='chat-message'>Chat Message</div>
           </div>
         </div>
-        {/* Example chat */}
+        {/* Example contact component */}
       </section>
       <section className='chat-container'>
-        <header className='chat-info'>
-          <div className='chat-name'>Indian</div>
-          <div className='chat-status'>Online</div>
+        <header className='d-flex'>
+          <img className='chat-picture' src='https://hips.hearstapps.com/hmg-prod/images/frenchie-the-boys-season-3-1655812929.jpg?crop=0.253xw:0.380xh;0.580xw,0.0484xh&resize=980:*' alt='nick from the office' />
+          <div className='chat-info'>
+            <div className='chat-name'>Contact name</div>
+            <div className='chat-status'>Status</div>
+          </div>
         </header>
         <div className='chat-messages-container'>
-          <div className='messages-container'>
-            {messages.map((message) => (
-              <div
-                key={message.id}
-                className={`message ${message.userId === currentUserId ? 'message-sender' : 'message-receiver'}`}
-              >
-                {message.text}
-              </div>
-            ))}
+          <div className='messages-box d-flex flex-column-reverse'>
+            {/* <div className='messages-container'>
+              {messages.map((message) => (
+                <div
+                  key={message.id}
+                  className={`message ${message.userId === currentUserId ? 'message-sender' : 'message-receiver'}`}
+                >
+                  {message.text}
+                </div>
+              ))}
+            </div> */}
+            <div className='messages-container'>Oi cunt</div>
+            <div className='messages-container'>Oi cunt</div>
           </div>
           <form className='message-form'>
             <input className='message-input' type='text' />
@@ -59,7 +66,7 @@ function ChatsPage({ messages, currentUserId }: MessageProps) {
       </section>
       {showAboutContainer && 
       <section className='about-container'>
-        <img src='https://static1.srcdn.com/wordpress/wp-content/uploads/2023/02/nelson-franklin-pointing-his-finger-on-the-office.jpg' alt='group-pic' />
+        <img src='https://hips.hearstapps.com/hmg-prod/images/frenchie-the-boys-season-3-1655812929.jpg?crop=0.253xw:0.380xh;0.580xw,0.0484xh&resize=980:*' alt='group-pic' />
         <div>Group Title</div>
       </section>
       }
